68f555489f1bf3f336170c9bf40a5fa2a3c54c04
elledkim <danikimnj@gmail.com>
2023-12-05 23:58:57 -0500

    12/5 2

--- Lab 3/script.js
+++ Lab 3/script.js
@@ -1,11 +1,33 @@
 var cells = document.querySelectorAll('.cell');
 var current = 'X';
 
+var scores = { 'X': 0, 'O': 0 };
+var scoreX = document.getElementById('scoreX');
+var scoreO = document.getElementById('scoreO');
+
 cells.forEach(function(cell) {
     cell.onclick = function() {
         if (cell.textContent === '') {
             cell.textContent = current;
-            current = current === 'X' ? 'O' : 'X';
+
+            if (checkWinner(current)) {
+                alert(current + ' wins!');
+                scores[current]++
+                
+                updatescore();
+                restartGame();
+            }
+
+            else if (isTie()) {
+                alert('It\'s a tie!');
+                restartGame();
+            }
+
+            if (current === 'X') {
+                current = 'O';
+            } else {
+                current = 'X';
+            }
         }
     };
 });
@@ -16,3 +38,34 @@ function restartGame() {
     });
     current = 'X';
 }
+
+function checkWinner(player) {
+    return (
+        // rows
+        (cells[0].textContent === player && cells[1].textContent === player && cells[2].textContent === player) ||
+        (cells[3].textContent === player && cells[4].textContent === player && cells[5].textContent === player) ||
+        (cells[6].textContent === player && cells[7].textContent === player && cells[8].textContent === player) ||
+
+        // columns
+        (cells[0].textContent === player && cells[3].textContent === player && cells[6].textContent === player) ||
+        (cells[1].textContent === player && cells[4].textContent === player && cells[7].textContent === player) ||
+        (cells[2].textContent === player && cells[5].textContent === player && cells[8].textContent === player) ||
+
+        // diags
+        (cells[0].textContent === player && cells[4].textContent === player && cells[8].textContent === player) ||
+        (cells[2].textContent === player && cells[4].textContent === player && cells[6].textContent === player)
+    );
+}
+
+function isTie() {
+    return Array.from(cells).every(function(cell) {
+        return cell.textContent === 'X' || cell.textContent === 'O';
+    });
+}
+
+function updatescore() {
+    scoreX.textContent = 'X: ' + scores['X'];
+    scoreO.textContent = 'O: ' + scores['O'];
+}
+
+updatescore();
